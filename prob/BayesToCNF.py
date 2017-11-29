b62087b58a0783b04f94facf9b49d04a8cb17447
thierryderuyttere <thierryderuyttere@hotmail.com>
2017-11-29 13:47:54 +0100

    enc2

--- prob/BayesToCNF.py
+++ prob/BayesToCNF.py
@@ -1,13 +1,16 @@
 from BayesModel import BayesModel
 from CNFEnc1 import CNFEnc1
+from CNFEnc2 import CNFEnc2
 
 def loadModel(file):
     return BayesModel(file)
-
 
 def toEnc1(bayes):
     cnf = CNFEnc1(bayes)
     cnf.convert()
     return cnf
 
-
+def toEnc2(bayes):
+    cnf = CNFEnc2(bayes)
+    cnf.convert()
+    return cnf
