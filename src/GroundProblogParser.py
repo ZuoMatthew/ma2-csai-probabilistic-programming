b761ad9d8f9f28a3b37170e0598d47fc3e81b87a
Armin <halilovic.armin@hotmail.com>
2018-02-11 21:08:36 +0100

    Expanded parser for ground Problog programs. Ready for conversion to CNF.

--- src/GroundProblogParser.py
+++ src/GroundProblogParser.py
@@ -14,29 +14,39 @@ class GroundProblogParser():
     def _grammar(self):
         # statement, term, atom, conjunction, disjunction, rule
         return Grammar(r"""
-            program       = ~"\n*" clause*
-            clause        = comment / fact / rule
+            program     = newlines clause*
+            clause      = (comment newlines) / ((prob_decls / rule / fact) end_clause)
             
-            comment       = "%" line ~"\n*"
+            fact        = term
             
-            fact          = term end_clause
+            rule        = term turnstile (conjunction / disjunction / term)
+            # which conjunction to use? see later when defining visitor that will build the CNF
+            #conjunction = term (comma conjunction)?
+            #conjunction = (term comma conjunction) / term
+            conjunction = term (comma term)*
+            disjunction = (term semicolon disjunction) / term
+
+            prob_decls  = prob_decl (semicolon prob_decls)?
+            prob_decl   = probability doublecolon (rule / fact)
             
-            rule          = term ~" *:- *" predicate* end_clause
-            predicate     = conjunction / disjunction
-            conjunction   = (term ~" *, *" conjunction) / term
-            disjunction   = (term ~" *; *" disjunction) / term
+            term        = negation? functor ("(" arguments ")")?
+            functor     = word
+            arguments   = (term comma arguments) / term
             
-            compound_term = word
-            term          = (functor "(" arguments ")") / functor
-            functor       = word
-            arguments     = (word "," arguments) / word
-            
-            line          = ~"[a-zA-Z0-9_ ]*"
-            word          = ~"[a-zA-Z0-9_]+"
-            end_clause    = ~" *\. *\n*"
-            number        = ~"[0-9]*"
-            variable      = ~"[A-Z _]"
-            newline       = ~"\s"
+            newlines    = ~"\n*"
+            comment     = ~"%.*"
+            word        = ~"[a-zA-Z0-9_]+"
+            end_clause  = ~" *\. *\n*"
+            number      = ~"[0-9]*"
+            variable    = ~"[A-Z_][a-zA-Z0-9_]*"
+            negation    = ~" *" "\+" ~" *"
+            comma       = ~" *, *"
+            semicolon   = ~" *; *"
+            doublecolon = ~" *:: *"
+            turnstile   = ~" *:- *"
+            probability = decimal / fraction
+            decimal     = ~"[0-9]*\.[0-9]*"
+            fraction    = (number "/" number)
         """)
 
     def parse(self, program):
