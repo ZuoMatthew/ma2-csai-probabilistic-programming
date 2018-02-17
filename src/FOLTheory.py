c192dba059f9fcfcd4c52cc717b3a2e96f0658b0
Armin <halilovic.armin@hotmail.com>
2018-02-17 23:55:18 +0100

    Completed FOL to CNF conversion and added a couple of test cases

--- src/FOLTheory.py
+++ src/FOLTheory.py
@@ -18,7 +18,7 @@ class LogicFormula:
 
 class Atom(LogicFormula):
     """ An atom is of the form p(t_1, ..., t_n) where p is a predicate of arity n and the t_i are terms. """
-    def __init__(self, predicate, terms):
+    def __init__(self, predicate, terms=[]):
         self.predicate = predicate
         self.terms = terms
         self.arity = len(terms)
@@ -30,7 +30,7 @@ class Atom(LogicFormula):
         return out
 
     def __eq__(self, other):
-        return self.predicate == other.predicate and self.terms == other.terms
+        return isinstance(other, Atom) and self.predicate == other.predicate and self.terms == other.terms
 
     @staticmethod
     def create_from_problog_term(term):
@@ -49,6 +49,9 @@ class Negation(LogicFormula):
 
     def __str__(self):
         return "¬(" + str(self.formula) + ")"
+
+    def __eq__(self, other):
+        return isinstance(other, Negation) and self.formula == other.formula
 
     def to_cnf(self):
         """ Convert this formula to Conjunctive Normal Form. """
@@ -77,58 +80,102 @@ class Negation(LogicFormula):
 class Conjunction(LogicFormula):
     """ Represents the logical connective '∧'. """
     def __init__(self, formulas):
-        self.formulas = formulas
+        self.formulas = []
+        # Lift nested Conjunctions' formulas up into this one.
+        for f in formulas:
+            if isinstance(f, Conjunction):
+                self.formulas += f.formulas
+            else:
+                self.formulas.append(f)
 
     def __str__(self):
         return "(" + " ∧ ".join([str(f) for f in self.formulas]) + ")"
 
+    def __eq__(self, other):
+        if not isinstance(other, Conjunction):
+            return False
+        for f in self.formulas:
+            if f not in other.formulas:
+                return False
+        for f in other.formulas:
+            if f not in self.formulas:
+                return False
+        return True
+
     def to_cnf(self):
         """ Convert this formula to Conjunctive Normal Form. """
-        # Formula is of form (P ∧ ... ∧ Q)
-        # P.to_cnf() must have the form P1 ∧ P2 ∧ ... ∧ Pm
-        # Q.to_cnf() must have the form Q1 ∧ Q2 ∧ ... ∧ Qn
-        # where all Pi and Qi are disjunctions of literals. => return P1 ∧ ... ∧ Pm ∧ Q1 ∧ ... ∧ Qm
-        return Conjunction([f.to_cnf() for f in self.formulas])  # (P.to_cnf() ∧ ... ∧ Q.to_cnf())
+        if len(self.formulas) == 1:
+            # If there is just one formula in the Conjunction, then return the CNF of that formula.
+            return self.formulas[0].to_cnf()
+        else:
+            # Formula is of form (P ∧ ... ∧ Q)
+            # P.to_cnf() must have the form P1 ∧ P2 ∧ ... ∧ Pm
+            # Q.to_cnf() must have the form Q1 ∧ Q2 ∧ ... ∧ Qn
+            # where all Pi and Qi are disjunctions of literals. => return P1 ∧ ... ∧ Pm ∧ Q1 ∧ ... ∧ Qm
+            return Conjunction([f.to_cnf() for f in self.formulas])  # (P.to_cnf() ∧ ... ∧ Q.to_cnf())
 
 
 class Disjunction(LogicFormula):
     """ Represents the logical connective '∨'. """
     def __init__(self, formulas):
-        self.formulas = formulas
+        self.formulas = []
+        # Lift nested Disjunction' formulas up into this one.
+        for f in formulas:
+            if isinstance(f, Disjunction):
+                self.formulas += f.formulas
+            else:
+                self.formulas.append(f)
 
     def __str__(self):
         return "(" + " ∨ ".join([str(f) for f in self.formulas]) + ")"
 
-    def add_formula(self, formula):
-        self.formulas.append(formula)
+    def __eq__(self, other):
+        if not isinstance(other, Disjunction):
+            return False
+        for f in self.formulas:
+            if f not in other.formulas:
+                return False
+        for f in other.formulas:
+            if f not in self.formulas:
+                return False
+        return True
 
     def to_cnf(self):
         """ Convert this formula to Conjunctive Normal Form. """
-        # Formula is of form (P ∨ ... ∨ Q)
-        # P.to_cnf() must have the form P1 ∧ P2 ∧ ... ∧ Pm
-        # Q.to_cnf() must have the form Q1 ∧ Q2 ∧ ... ∧ Qn
-        # where all Pi and Qi are disjunctions of literals
-        # We need a CNF formula equivalent to (P1 ∧ P2 ∧ ... ∧ Pm) ∨ (Q1 ∧ Q2 ∧ ... ∧ Qn)
-        # => return (P1 v Q1) ^ (P1 v Q2) ^ ... ^ (P1 v Qn)
-        #         ^ (P2 v Q1) ^ (P2 v Q2) ^ ... ^ (P2 v Qn)
-        #         ^ ...
-        #         ^ (Pm v Q1) ^ (Pm v Q1) ^ ... ^ (Pm V Qn)
-        # In other words, apply the distributive law where a disjunction occurs over a conjunction.
         if len(self.formulas) == 1:
-            return self.formulas[0]
+            # If there is just one formula in the Disjunction, then return the CNF of that formula.
+            return self.formulas[0].to_cnf()
         else:
-            p = self.formulas.pop(0).to_cnf()
-            q = self.formulas.pop(0).to_cnf()
-            p_formulas = [p] if isinstance(p, (Atom, Negation)) else p.formulas
-            q_formulas = [q] if isinstance(q, (Atom, Negation)) else q.formulas
+            # Formula is of form (P ∨ ... ∨ Q)
+            # P.to_cnf() must have the form P1 ∧ P2 ∧ ... ∧ Pm
+            # Q.to_cnf() must have the form Q1 ∧ Q2 ∧ ... ∧ Qn
+            # where all Pi and Qi are disjunctions of literals
+            # We need a CNF formula equivalent to (P1 ∧ P2 ∧ ... ∧ Pm) ∨ (Q1 ∧ Q2 ∧ ... ∧ Qn)
+            # => return (P1 ∨ Q1) ∧ (P1 ∨ Q2) ∧ ... ∧ (P1 ∨ Qn)
+            #         ∧ (P2 ∨ Q1) ∧ (P2 ∨ Q2) ∧ ... ∧ (P2 ∨ Qn)
+            #         ∧ ...
+            #         ∧ (Pm ∨ Q1) ∧ (Pm ∨ Q1) ∧ ... ∧ (Pm ∨ Qn)
+            # In other words, apply the distributive law where a disjunction occurs over a conjunction.
+            formulas_cnf = [f.to_cnf() for f in self.formulas]
 
-            formulas = []
-            for f1 in p_formulas:
-                for f2 in q_formulas:
-                    formulas.append(Disjunction([f1, f2]))
-            # create conjunction of new disjunctions
-            conj = Conjunction(formulas)
-            return Disjunction(self.formulas + [conj]).to_cnf()
+            if len([f for f in formulas_cnf if isinstance(f, Conjunction)]) == 0:
+                # If there is no Conjunction in this Disjunction, the formula is already in CNF
+                return Disjunction(formulas_cnf)
+
+            # Build up the result as a Conjunction of Disjunctions
+            result = Conjunction([formulas_cnf.pop(0)])
+            while len(formulas_cnf) > 0:
+                f = formulas_cnf.pop(0)
+                f_formulas = [f] if isinstance(f, (Atom, Negation)) else f.formulas
+
+                disjunctions = []
+                for p in result.formulas:
+                    for q in f_formulas:
+                        disjunctions.append(Disjunction([p, q]))
+
+                result = Conjunction(disjunctions)
+
+            return result.to_cnf()
 
 
 class Equivalence(LogicFormula):
@@ -138,7 +185,11 @@ class Equivalence(LogicFormula):
         self.rhs = rhs
 
     def __str__(self):
-        return "(" + str(self.lhs) + " ↔ " + str(self.rhs) + ")"
+        return "e(" + str(self.lhs) + " ↔ " + str(self.rhs) + ")"
+
+    def __eq__(self, other):
+        return isinstance(other, Equivalence) and \
+               (self.lhs == other.lhs and self.rhs == other.rhs or self.lhs == other.rhs and self.rhs == other.lhs)
 
     def to_cnf(self):
         """ Convert this formula to Conjunctive Normal Form. """
@@ -190,10 +241,7 @@ class FOLTheory:
             if i == -1:
                 rules.append((head, body))
             else:
-                if isinstance(rules[i][1], Disjunction):
-                    rules[i][1].add_formula(body)
-                else:
-                    rules[i] = (rules[i][0], Disjunction([rules[i][1], body]))
+                rules[i] = (rules[i][0], Disjunction([rules[i][1], body]))
 
         for (head, body) in rules:
             theory.add_formula(Equivalence(head, body))
---
+++ src/test-FOL-to-CNF.py
@@ -0,0 +1,35 @@
+from FOLTheory import *
+
+
+def compare(formula, expected):
+    print("Formula: ", formula)
+    print("CNF:     ", formula.to_cnf())
+    print("Expected:", expected)
+    print("Equals:  ", formula.to_cnf() == expected)
+    print()
+
+
+# P || Q || (R && !S)
+formula = Disjunction([Atom('P'), Atom('Q'), Conjunction([Atom('R'), Negation(Atom('S'))])])
+# (P || Q || R) && (P || Q || !S)
+expected = Conjunction([Disjunction([Atom('P'), Atom('Q'), Atom('R')]), Disjunction([Atom('P'), Atom('Q'), Negation(Atom('S'))])])
+compare(formula, expected)
+
+# P || (Q && R) || (S && T && U) || V
+formula = Disjunction([Atom('P'), Conjunction([Atom('Q'), Atom('R')]), Conjunction([Atom('S'), Atom('T'), Atom('U')]), Atom('V')])
+# (P || Q || S || V) && (P || Q || T || V) && (P || Q || U || V) &&
+# (P || R || S || V) && (P || R || T || V) && (P || R || U || V)
+expected = Conjunction([
+    Disjunction([Atom('P'), Atom('Q'), Atom('S'), Atom('V')]), Disjunction([Atom('P'), Atom('Q'), Atom('T'), Atom('V')]), Disjunction([Atom('P'), Atom('Q'), Atom('U'), Atom('V')]),
+    Disjunction([Atom('P'), Atom('R'), Atom('S'), Atom('V')]), Disjunction([Atom('P'), Atom('R'), Atom('T'), Atom('V')]), Disjunction([Atom('P'), Atom('R'), Atom('U'), Atom('V')])
+])
+compare(formula, expected)
+
+# P || Q || (R && S) || (T && U)
+formula = Disjunction([Atom('P'), Atom('Q'), Conjunction([Atom('R'), Atom('S')]), Conjunction([Atom('T'), Atom('U')])])
+# (T || Q || R || P) && (T || Q || S || P)_ && (U || Q || R || P) && (U || Q || S || P)
+expected = Conjunction([
+    Disjunction([Atom('T'), Atom('Q'), Atom('R'), Atom('P')]), Disjunction([Atom('T'), Atom('Q'), Atom('S'), Atom('P')]),
+    Disjunction([Atom('U'), Atom('Q'), Atom('R'), Atom('P')]), Disjunction([Atom('U'), Atom('Q'), Atom('S'), Atom('P')])
+])
+compare(formula, expected)
