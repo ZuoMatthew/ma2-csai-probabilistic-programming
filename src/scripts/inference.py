0a3dabebf2004314e8f4482a5d9390743493fd27
thierryderuyttere <thierryderuyttere@hotmail.com>
2018-03-23 17:55:04 +0100

    updating paper 2

--- src/scripts/inference.py
+++ src/scripts/inference.py
@@ -3,7 +3,7 @@ from InferenceEngine import InferenceEngine
 
 
 if __name__ == '__main__':
-    filename = "../files/test.grounded.pl"
+    filename = "files/test.grounded.pl"
     program = util.file_to_string(filename)
 
     engine = InferenceEngine()
