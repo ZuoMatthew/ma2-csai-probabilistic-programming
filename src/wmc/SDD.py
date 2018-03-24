87da9ce60a9c717f270b82206be3c6d53c8cbb83
thierryderuyttere <thierryderuyttere@hotmail.com>
2018-03-24 19:41:06 +0100

    Added weights to sdd

--- src/wmc/SDD.py
+++ src/wmc/SDD.py
@@ -12,8 +12,23 @@ class SDD(WeightedModelCounter):
     def __init__(self):
         pass
 
+
+    def get_weights_from_cnf(self, filename):
+        cnf = open(os.path.abspath(filename), "r")
+        lines = cnf.readlines()
+        weights = []
+        for line in lines:
+            if "weight" in line:
+                splitted = line.strip("\n").split(" ")
+                weights = splitted[2:-1]
+                #print(weights)
+                weights = [float(w) for w in weights]
+
+        return weights
+
     # Based on http://pysdd.readthedocs.io/en/latest/examples/model_counting.html
     def do_model_count(self, filename):
+        print(filename)
         # 1. create VTREE
         vtree_path = MiniC2D("").create_vtree(filename)
         vtree = Vtree.from_file(bytes(vtree_path.encode('utf-8')))
@@ -24,9 +39,18 @@ class SDD(WeightedModelCounter):
         # 2. read CNF
         root = sdd.read_cnf_file(bytes(os.path.abspath(filename).encode('utf-8')))
 
-        # Weighted model counting
-        wmc = root.wmc(log_mode=True)
+        # model counting
+        wmc = root.wmc(log_mode=False)
+
+        # Add weights
+
+        lits = [sdd.literal(i) for i in range(1, sdd.var_count() + 1)]
+        weights = self.get_weights_from_cnf(filename)
+
+        for i in range(len(lits)):
+
+            wmc.set_literal_weight(lits[i], weights[2*i])
+            wmc.set_literal_weight(-lits[i], weights[2 * i + 1])
+
         w = wmc.propagate()
-
-
         return w
