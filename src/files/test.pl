70bd087692a3da248b735f3d6a966ffbd4e488ca
Thierry Deruyttere <thierryderuyttere@hotmail.com>
2018-03-29 14:28:16 +0200

    fixing dem bugs

--- src/files/test.pl
+++ src/files/test.pl
@@ -1,8 +1,21 @@
-0.3::a; 0.54::b.
-0.4::c; 0.4::d :- a, b.
+person(john).
+person(mary).
 
-evidence(b, false).
+0.7::burglary.
+0.2::earthquake.
 
-query(a).
-query(c).
-query(d).
+0.9::alarm :- burglary, earthquake.
+0.8::alarm :- burglary, \+earthquake.
+0.1::alarm :- \+burglary, earthquake.
+
+0.8::calls(X) :- alarm, person(X).
+0.1::calls(X) :- \+alarm, person(X).
+
+evidence(calls(john),true).
+evidence(calls(mary),true).
+
+query(burglary).
+query(earthquake).
+
+
+
--- src/ground_problog/GroundProblog.py
+++ src/ground_problog/GroundProblog.py
@@ -107,7 +107,7 @@ class GroundProblog:
                         new_clauses.append(fake_head_term)
 
                         # Create a new head with no weights
-                        new_head_wo_prob = Term(head.name)
+                        new_head_wo_prob = Term(head.name, arguments=head.arguments)
 
                         new_rule = Rule(new_head_wo_prob, clause.body + [fake_head_term])
                         new_clauses.append(new_rule)
