90c5ce2fb87e4ebbce24e7d432fd557f589ef528
Thierry Deruyttere <thierryderuyttere@hotmail.com>
2018-04-01 22:14:23 +0200

    change literal names

--- src/files/problog/converted_cancer.pl
+++ src/files/problog/converted_cancer.pl
@@ -14,23 +14,20 @@ cancer:- pollution("high"), smoker, alpha_cancer_3.
 t(_)::alpha_cancer_4.
 cancer:- pollution("high"), \+smoker, alpha_cancer_4.
 
-% t(_)::xray("positive"); t(_)::xray("negative") :- cancer.
-% t(_)::xray("positive"); t(_)::xray("negative") :- \+cancer.
+t(_)::alpha_xray_1("positive").
+xray_1("positive"):- alpha_xray_1("positive"), \+xray_1("negative"), cancer.
 
-t(_)::alpha_xray("positive")_1.
-xray("positive")_1 :- alpha_xray("positive")_1, \+xray("negative")_1, cancer.
+t(_)::alpha_xray_1("negative").
+xray_1("negative"):- alpha_xray_1("negative"), \+xray_1("positive"), cancer.
 
-t(_)::alpha_xray("negative")_1.
-xray("negative")_1:- alpha_xray("negative")_1, \+xray("positive")_1, cancer.
+t(_)::alpha_xray_2("positive").
+xray_2("positive") :- alpha_xray_2("positive"), \+ xray_2("negative"), \+cancer.
 
-t(_)::alpha_xray("positive")_2.
-xray("positive")_2 :- alpha_xray("positive")_2, \+ xray("negative")_2, \+cancer.
+t(_)::alpha_xray_2("negative").
+xray_2("negative") :- alpha_xray_2("negative"), \+ xray_2("positive"), \+cancer.
 
-t(_)::alpha_xray("negative")_2.
-xray("negative")_2 :- alpha_xray("negative")_2, \+ xray("positive")_2, \+cancer.
-
-xray("positive") :- xray("positive")_1, xray("positive")_2.
-xray("negative") :- xray("negative")_1, xray("negative")_2.
+xray("positive") :- xray_1("positive"), xray_2("positive").
+xray("negative") :- xray_1("negative"), xray_2("negative").
 
 t(_)::dyspnoea_1.
 dyspnoea:- cancer, dyspnoea_1.
